500939a01021fb4942c3eef07dd0da3d69d0133c
Mohsin-khan540 <arhamk4555@gmail.com>
2025-09-12 23:11:03 +0500

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Mohsin khan on 12/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
